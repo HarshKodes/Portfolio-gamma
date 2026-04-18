5adfcef2b664d2bb53b20f0d73fe7c0b52063cf6
HarshKodes <yharshpratap9644@gmail.com>
2026-04-18 15:06:10 +0530

    Initial React + CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import './styles/navbar.css'
+
+function App() {
+  return (
+      <>
+        <Navbar />
+        {/* Other sections will go here */}
+      </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+            </ul>
+            <button className="talk-btn">Let’s Talk</button>
+        </nav>
+    )
+}
